449d8eec60ce6304b4b5fdb9fe545c8dc760d6f8
Stefan Scherer <scherer_stefan@icloud.com>
2018-09-11 07:19:15 +0200

    Fix checksums

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = 'archiver'
 $url = 'https://github.com/mholt/archiver/releases/download/v2.1/archiver_windows_386.exe'
 $url64 = 'https://github.com/mholt/archiver/releases/download/v2.1/archiver_windows_amd64.exe'
-$checksum = '0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5'
-$checksum64 = '0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5'
+$checksum = 'a826769480e309adb2578d2dd19930dee2e7a9ecf52f9760df8af11430f4052a'
+$checksum64 = 'a9e91b3adef6f89e6eb3d846ac5d49b78e0c28d8ef446392c37c26b8ec4fcfd2'
 $checksumType = 'sha256'
 $checksumType64 = $checksumType
 
